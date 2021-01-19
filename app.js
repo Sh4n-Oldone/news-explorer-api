d83245a3b83aa9cffddcac7abcb0432348035090
Alex Pavlov <halfer.orc@gmail.com>
2021-01-19 20:41:14 +0300

    cors v5

--- app.js
+++ app.js
@@ -30,16 +30,11 @@ mongoose.connect(`mongodb://${NODE_ENV === 'production' ? MONGO_LINK : 'localhos
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: '*',
+  methods: 'GET, POST, DELETE',
+}));
 app.options('*', cors());
-
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
