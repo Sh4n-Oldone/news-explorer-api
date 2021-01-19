6c85e7e132e0eac4e01540570bda76b18425c1d6
Alex Pavlov <halfer.orc@gmail.com>
2021-01-19 20:11:03 +0300

    cors v4

--- app.js
+++ app.js
@@ -30,16 +30,16 @@ mongoose.connect(`mongodb://${NODE_ENV === 'production' ? MONGO_LINK : 'localhos
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(cors({ origin: true }));
-// app.options('*', cors());
+app.use(cors({ origin: '*' }));
+app.options('*', cors());
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
